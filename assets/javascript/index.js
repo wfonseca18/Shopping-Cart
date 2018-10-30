d3a8fa4bf15cdcb2b00be726eec4debbcf4b18d9
Wesley Fonseca <wfonseca18@gmail.com>
2018-10-30 10:26:39 -0400

    Adding click functionality

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,20 +1,86 @@
 
 var productList = [
-    'laptop',
-    'mouse',
-    'monitor',
-    'keyboard',
-    'dock',
-    'headphones',
-    'power supply',
-    'batteries'
+    {
+        'category': 'Technology',
+        'name': 'Laptop'
+    },
+    {
+        'category': 'Technology',
+        'name': 'Mouse'
+    },
+    {
+        'category': 'Technology',
+        'name': 'Monitor'
+    },
+    {
+        'category': 'Technology',
+        'name': 'Keyboard'
+    },
+    {
+        'category': 'Technology',
+        'name': 'Dock'
+    },
+    {
+        'category': 'Supplies',
+        'name': 'Pencil'
+    },
+    {
+        'category': 'Supplies',
+        'name': 'Pen'
+    },
+    {
+        'category': 'Supplies',
+        'name': 'Tape'
+    },
+    {
+        'category': 'Supplies',
+        'name': 'Staples'
+    },
+    {
+        'category': 'Refreshments',
+        'name': 'Chips'
+    },
+    {
+        'category': 'Refreshments',
+        'name': 'Candy Bars'
+    },
+    {
+        'category': 'Refreshments',
+        'name': 'Coke'
+    },
+    {
+        'category': 'Refreshments',
+        'name': 'Sprite'
+    }
 ];
 
-for(let i = 0; i < productList.length; i++){
-    $('.item-list').append(`<li class="p-2"><button class="btn-${i}">${productList[i]}</button></li>`);
-    $(`.btn-${i}`).on('click', function(){
-        $('.cart-list').append(`<li class="p-2"><button class="btn-${i}">${productList[i]}</button></li>`);
-        // $(`#btn-${i}`).append($(`.btn-${i}`));
+var categories = [];
+
+for (let i = 0; i < productList.length; i++) {
+    $('.item-list').append(`<button class="m-2 btn-${i}">${productList[i].name}</button>`);
+    $(`.btn-${i}`).on('click', function () {
+        $('.cart-list').append(`<button id="btn-${i}" class="m-2 btn-${i}">${productList[i].name}</button>`);
     });
 }
+
+productList.forEach(item => {
+    let exists = false;
+    categories.forEach(category => {
+        if(item.category === category){exists = true;}
+    });
+    if(!exists){
+        categories.push(item.category);
+        $('.categories').append(`<button class="m-2" id="${item.category}">${item.category}</button>`);
+        $(`#${item.category}`).on('click', function(){
+            console.log(item.category);
+            let products = $('.item-list');
+            products.forEach(product => {
+                // 
+            });
+
+        });
+        exists = false;
+    }
+});
+
 
